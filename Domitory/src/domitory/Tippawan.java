7079cc8a1f9ea05f9d07496d1832f815ca56154c
Administrator <Administrator@10.240.9.32>
2014-07-02 09:26:13 +0700

    Tippawan

---
+++ Domitory/src/domitory/Tippawan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package domitory;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Tippawan {
+    String name= "Tippawan Plodprong";
+    String stuid="55021306";
+    
+}
