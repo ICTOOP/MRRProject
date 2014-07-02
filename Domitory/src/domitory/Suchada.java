832f2c2937e80be8e9f66a45f12767613ec1492c
inewgaza@gmail.com <>
2014-07-02 09:41:08 +0700

    Suchada

---
+++ Domitory/src/domitory/Suchada.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package domitory;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Suchada {
+   String name = "suchada uppananchai";
+    String studentid = "55025928";
+}
